85e84d623616a6a49c90bab507827d8a19b5bc72
dmyneni98 <mdhiraja@gmail.com>
2022-10-11 14:59:51 -0500

    new

--- SimpleFav/ProfileViewController.swift
+++ SimpleFav/ProfileViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var BDayText: UILabel!
+    
+    @IBOutlet weak var heightText: UITextField!
+    
+    @IBOutlet weak var weightText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
